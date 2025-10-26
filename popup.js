1e2e16f5b4dc34cfd71f22c21ee96516f1d4d785
vikrantaroraa <arora.veshu@gmail.com>
2025-10-26 17:17:26 +0530

    feat:- added the feature to onlly start recoding when the capture btn is pressed instead of starting automatically on opening extension popup and added some styles to popup

--- popup.js
+++ popup.js
@@ -1,3 +1,6 @@
+const recordTab = document.getElementById("tab");
+const recordScreen = document.getElementById("screen");
+
 const runCode = async () => {
   // Inject the content script into the active tab when the popup is opened
   const tab = await chrome.tabs.query({ active: true, currentWindow: true });
@@ -16,4 +19,40 @@ const runCode = async () => {
   });
 };
 
-runCode();
+//Check chrome storage if recording is already in progress
+const checkRecordingStatus = async () => {
+  const result = await chrome.storage.local.get(["recording", "type"]);
+  const recordingStatus = result.recording || false;
+  const recordingType = result.type || "";
+  console.log("Recording status:- ", recordingStatus, recordingType);
+  return [recordingStatus, recordingType];
+};
+
+const init = async () => {
+  const recordingState = await checkRecordingStatus();
+
+  if (recordingState[0] === true) {
+    if (recordingState[1] === "tab") {
+      recordTab.innerText = "Stop Recording Tab";
+    } else {
+      recordScreen.innerText = "Stop Recording Screen";
+    }
+  }
+
+  // Listen for when either of 'Record Tab' or 'Record Screen' buttons are clicked
+  const startRecording = async (type) => {
+    console.log(`Starting ${type} recording...`);
+  };
+
+  recordTab.addEventListener("click", async () => {
+    console.log("Record Tab button clicked");
+    startRecording("tab");
+  });
+
+  recordScreen.addEventListener("click", async () => {
+    console.log("Record Screen button clicked");
+    startRecording("screen");
+  });
+};
+
+init();
