23648272b564ce78f85645948e065b73a8cad1cb
vikrantaroraa <arora.veshu@gmail.com>
2025-11-11 19:46:56 +0530

    feat: added the feature to save the recorded video to local storage and play the save video when the page is opened or refreshed

--- video.js
+++ video.js
@@ -1,7 +1,22 @@
+const saveVideo = (videoUrl) => {
+  chrome.storage.local.set({ videoUrl });
+};
+
+// on page open, check if there is any recorded video in local storage and play it
+chrome.storage.local.get(["videoUrl"], (result) => {
+  console.log("Recorded video from storage:- ", result);
+  if (result.videoUrl) {
+    console.log("Play Recorded video from storage:- ", result);
+    playVideo(result);
+  }
+});
+
 const playVideo = (request) => {
   const videoElement = document.getElementById("recorded-video");
 
   const url = request?.videoUrl || request?.base64;
+  // save the videoin local storage
+  saveVideo(url);
   videoElement.src = url;
   videoElement.play();
 };
@@ -12,6 +27,7 @@ chrome.runtime.onMessage.addListener((request, sender) => {
     case "play-video":
       console.log("play video:- ", request);
       playVideo(request);
+      break;
     default:
       console.log("default");
   }
